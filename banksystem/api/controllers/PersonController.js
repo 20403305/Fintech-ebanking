21d751cf4b0866eea4d5dc15ce2ffb44299b736b
suyuxin <20403305@life.hkbu.edu.hk>
2021-04-05 12:09:33 +0800

    创建person_api ; 添加增删改查 ; 引用bulma

---
+++ banksystem/api/controllers/PersonController.js
@@ -0,0 +1,76 @@
+/**
+ * PersonController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    // action - create
+    create: async function(req, res) {
+
+        if (req.method == "GET") return res.view('person/create');
+
+        var person = await Person.create(req.body).fetch();
+
+        return res.status(201).json({ id: person.id });
+    },
+
+    // json function
+    json: async function(req, res) {
+
+        var everyones = await Person.find();
+
+        return res.json(everyones);
+    },
+
+    // action - list
+    list: async function(req, res) {
+
+        var everyones = await Person.find();
+
+        return res.view('person/list', { persons: everyones });
+    },
+
+    // action - read
+    read: async function(req, res) {
+
+        var thatPerson = await Person.findOne(req.params.id);
+
+        if (!thatPerson) return res.notFound();
+
+        return res.view('person/read', { person: thatPerson });
+    },
+
+    // action - delete 
+    delete: async function(req, res) {
+
+        var deletedPerson = await Person.destroyOne(req.params.id);
+
+        if (!deletedPerson) return res.notFound();
+
+        return res.ok();
+    },
+
+    // action - update
+    update: async function(req, res) {
+
+        if (req.method == "GET") {
+
+            var thatPerson = await Person.findOne(req.params.id);
+
+            if (!thatPerson) return res.notFound();
+
+            return res.view('person/update', { person: thatPerson });
+
+        } else {
+
+            var updatedPerson = await Person.updateOne(req.params.id).set(req.body);
+
+            if (!updatedPerson) return res.notFound();
+
+            return res.ok();
+        }
+    },
+
+};
--- banksystem/config/routes.js
+++ banksystem/config/routes.js
@@ -10,28 +10,37 @@
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * Make the view located at `views/homepage.ejs` your home page.            *
+     *                                                                          *
+     * (Alternatively, remove this and add an `index.html` file in your         *
+     * `assets` directory)                                                      *
+     *                                                                          *
+     ***************************************************************************/
 
-  '/': { view: 'pages/homepage' },
+    // '/': { view: 'pages/homepage' },
 
 
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * More custom routes here...                                               *
+     * (See https://sailsjs.com/config/routes for examples.)                    *
+     *                                                                          *
+     * If a request to a URL doesn't match any of the routes in this file, it   *
+     * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+     * not match any of those, it is matched against static assets.             *
+     *                                                                          *
+     ***************************************************************************/
+    'GET /person/create': 'PersonController.create',
+    'POST /person/create': 'PersonController.create',
 
+    'GET /': 'PersonController.list',
+    'GET /person': 'PersonController.list',
+    'GET /person/list': 'PersonController.list',
+    'GET /person/json': 'PersonController.json',
+    'GET /person/read/:id': 'PersonController.read',
+
+    'POST /person/delete/:id': 'PersonController.delete',
 
 };
