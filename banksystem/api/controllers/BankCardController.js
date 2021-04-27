58b877c99baef86df491b691c7e640150323a41e
suyuxin <20403305@life.hkbu.edu.hk>
2021-04-27 22:38:01 +0800

    创建空卡数字显示；普通用户不能进入list界面

--- banksystem/api/controllers/BankCardController.js
+++ banksystem/api/controllers/BankCardController.js
@@ -10,8 +10,18 @@ module.exports = {
     // action - list
     list: async function (req, res) {
         if (!req.session.username) return res.json("Please log in first");
-        
+
+//         ????问什么会不行
+//         member
+// error: Sending 500 ("Server Error") response: 
+//  Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+//     at ServerResponse.setHeader (_http_outgoing.js:518:11)
+//     at ServerResponse.header (C:\Users\HP\Desktop\7300project\banksystem\node_modules\express\lib\response.js:771:10)
+
+        // console.log(req.session.userrole)
+        if (req.session.userrole != "admin") res.json("Not permission");
         if (req.method == "GET") {
+
             var everyones = await BankCard.find();
 
             return res.view('bankcard/list', { bankcards: everyones, session: req.session });
